ddf84e41a2199652d53635b4179fd5f3a2143ef7
Mark <mark@remarkablemark.org>
2026-03-31 22:45:02 -0400

    chore(sprites): add card images

--- src/constants/sprite.ts
+++ src/constants/sprite.ts
@@ -1,4 +1,13 @@
+// enemy
 export const ARCHER = 'archer'
 export const HATCHLING = 'hatchling'
 export const REAVER = 'reaver'
 export const SOLDIER = 'soldier'
+
+// card
+export const BASTION = 'bastion'
+export const BLOOM = 'bloom'
+export const PIERCE = 'pierce'
+export const SURGE = 'surge'
+export const THORN = 'thorn'
+export const WILT = 'wilt'
