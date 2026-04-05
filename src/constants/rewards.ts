9fc93a9c25cbacdd482abed622992e6f84928ec9
Mark <mark@remarkablemark.org>
2026-04-05 18:18:32 -0400

    refactor(constants): tidy floor 1 and 2 rewards

--- src/constants/rewards.ts
+++ src/constants/rewards.ts
@@ -14,13 +14,13 @@ interface RewardDefinition {
 export const REWARD_DEFINITIONS: RewardDefinition[] = [
   // floor 1
   {
-    cards: ['sear1', 'pierce1', 'ward1', 'wilt1'],
+    cards: ['sear1', 'ward1', 'wilt1', 'leech1', 'heat1'],
     stats: [FULL_HEAL, MAX_HP],
   },
 
   // floor 2
   {
-    cards: ['bastion1', 'heavy1', 'heat1', 'leech1', 'double1'],
+    cards: ['bastion1', 'heavy1', 'double1', 'pierce1'],
     stats: [FULL_HEAL, MAX_HP],
     upgrade: true,
   },
